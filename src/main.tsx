16c6bbb7fe25683bb3e7325c84ae5a56c1a45937
TestProject <Rofweilveilmin@gmail.com>
2025-12-13 10:30:16 +0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { AuthProvider } from "./contexts/AuthContext";
 import './styles/globals.css';
 import './index.css';
 import { Toaster } from 'sonner@2.0.3';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <AuthProvider>
     <App />
+    </AuthProvider>
     <Toaster position="top-center" richColors />
   </React.StrictMode>
 );
