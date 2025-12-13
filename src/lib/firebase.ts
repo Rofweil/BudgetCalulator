83142900e339699858557ed08a43f5dc094ac719
TestProject <Rofweilveilmin@gmail.com>
2025-12-13 10:16:14 +0800

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,4 +1,5 @@
 import { initializeApp } from 'firebase/app';
+import { getAuth } from "firebase/auth";
 import { getDatabase } from 'firebase/database';
 
 const firebaseConfig = {
@@ -13,6 +14,7 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
 export const database = getDatabase(app);
 
 export async function sha256hex(msg: string): Promise<string> {
